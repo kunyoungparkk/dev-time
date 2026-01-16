24d3967ad2cd322834c3e922469dbba47931ee8c
kunyoungparkk <rjsdud3263@gmail.com>
2026-01-16 16:22:18 +0900

    make directory and routing structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
-    <div></div>
+    <div>
+    <Link href={'/timer'}>Timer</Link><br/>
+    <Link href={'/login'}>Login</Link><br/>
+    <Link href={'/signup'}>Signup</Link><br/>
+    <Link href={'/dashboard/0'}>dashboard</Link><br/>
+    <Link href={'/ranking'}>ranking</Link><br/>
+    <Link href={'/mypage'}>mypage</Link><br/>
+    </div>
   );
 }
---
+++ src/app/timer/@modal/todos-edit/[id]/page.tsx
@@ -0,0 +1,5 @@
+export default function TodoEdit() {
+  return (
+    <div>TodoEdit</div>
+  );
+}
