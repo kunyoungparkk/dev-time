e8ad391691ae0957fa6a96a994f4733cda57d327
kunyoungparkk <rjsdud3263@gmail.com>
2026-01-23 06:52:49 +0900

    Modal, Logo 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
+'use client';
+
+import {useState} from "react";
 import Link from "next/link";
-import { Button } from "@/components/ui/Button";
+import { Button, Modal, Logo } from "@/components/ui";
 import { DoubleChevronRightIcon } from "@/components/icons";
 
 export default function Home() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <div>
-    <Button variant="primary">Button</Button><br/><br/>
+    <Button variant="primary" onClick={() => setModalOpen(true)}>Button</Button><br/><br/>
     <Button variant="secondary">Button</Button><br/><br/>
     <Button variant="tertiary">Button</Button><br/> <br/>
+
+    <Modal
+      isOpen={modalOpen}
+      onClose={() => setModalOpen(false)}
+      title="Sample Modal"
+    />
+
+    <Logo /><br/><br/>
+    <Logo variant="large" /><br/><br/>
 
     <DoubleChevronRightIcon />
     <Link href={'/timer'}>Timer</Link><br/>
